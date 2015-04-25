906decbe99f9ac80b2e0a2c8d2549a724c11c244
vivaxy <xyxuye2007@126.com>
2015-04-25 11:44:31 +0800

    format

--- menu-animation/menu.js
+++ menu-animation/menu.js
@@ -1,19 +1,19 @@
-var xmlhttp=new XMLHttpRequest();
-xmlhttp.open("GET","menu.xml",false);
-xmlhttp.send();
-var xmlDoc=xmlhttp.responseXML;
-var menu = "<div class='menu'>" +
-	"<ul>";
-var menuList = xmlDoc.getElementsByTagName("menu")[0].getElementsByTagName("name");
-for (var i=0;i<menuList.length;i++){
-	menu = menu +
-		"<li><a href='" +
-		xmlDoc.getElementsByTagName("menu")[0].getElementsByTagName("link")[i].childNodes[0].nodeValue +
-		"'>" +
-		menuList[i].childNodes[0].nodeValue +
-		"</a></li>";
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'menu.xml', false);
+xhr.send();
+var xmlDoc = xhr.responseXML;
+var menu = '<div class="menu">' +
+    '<ul>';
+var menuList = xmlDoc.getElementsByTagName('menu')[0].getElementsByTagName('name');
+for (var i = 0; i < menuList.length; i++) {
+    menu = menu +
+        '<li><a href="' +
+        xmlDoc.getElementsByTagName('menu')[0].getElementsByTagName('link')[i].childNodes[0].nodeValue +
+        '">' +
+        menuList[i].childNodes[0].nodeValue +
+        '</a></li>';
 }
 menu = menu +
-	"</ul>" +
-	"</div>";
+    '</ul>' +
+    '</div>';
 document.write(menu);
--- menu-animation/menu4.js
+++ menu-animation/menu4.js
@@ -1,13 +1,13 @@
-var xmlhttp=new XMLHttpRequest();
-xmlhttp.open("GET","menu.xml",false);
-var menu = "";
-xmlhttp.send();
-var xmlDoc=xmlhttp.responseXML;
-var menuList = xmlDoc.getElementsByTagName("menu")[0].getElementsByTagName("name");
-for (var i=0;i<menuList.length;i++){
-	menu = menu +
-		"<div class='menu'>" +
-		menuList[i].childNodes[0].nodeValue +
-		"</div>";
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'menu.xml', false);
+var menu = '';
+xhr.send();
+var xmlDoc = xhr.responseXML;
+var menuList = xmlDoc.getElementsByTagName('menu')[0].getElementsByTagName('name');
+for (var i = 0; i < menuList.length; i++) {
+    menu = menu +
+        '<div class="menu">' +
+        menuList[i].childNodes[0].nodeValue +
+        '</div>';
 }
 document.write(menu);
