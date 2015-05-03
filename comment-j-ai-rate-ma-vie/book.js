8872adba9f4577ce872b299a42842ab1a9573989
vivaxy <xyxuye2007@126.com>
2015-05-03 14:40:19 +0800

    fix fullpage

--- comment-j-ai-rate-ma-vie/book.js
+++ comment-j-ai-rate-ma-vie/book.js
@@ -1,17 +1,14 @@
-// TODO load image before display
 $(document).ready(function () {
-    $.fn.fullpage({
-        slidesColor: ['#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2', '#f2f2f2'],
-        anchors: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19'],
-        menu: '#menu'
-    });
+    $('#full-page').fullpage();
 });
-$("img").ready(function () {
-    $("img").each(function () {
-        if (this.height / this.width > window.innerHeight / window.innerWidth) {
-            this.height = window.innerHeight;
-        } else {
-            this.width = window.innerWidth;
-        }
-    });
+
+var $img = $('img');
+$img.ready(function () {
+    var width = $img.width(),
+        height = $img.height();
+    if (height / width > window.innerHeight / window.innerWidth) {
+        $img.height(window.innerHeight);
+    } else {
+        $img.width(window.innerWidth);
+    }
 });
