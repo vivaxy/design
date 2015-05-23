7709e3cb8f2695870c4eb10d791d936249b35708
vivaxy <xyxuye2007@126.com>
2015-05-23 10:42:41 +0800

    number-animation

---
+++ number-animation/index.js
@@ -0,0 +1,22 @@
+/**
+ * @since 150523 10:36
+ * @author vivaxy
+ */
+
+var animate = function () {
+    new NumberAnimation({
+        container: document.querySelector('.number1'),
+        targetNumber: 12345.67
+    });
+
+    new NumberAnimation({
+        container: document.querySelector('.number2'),
+        targetNumber: 54321.09
+    });
+};
+
+window.addEventListener('click', function () {
+    animate();
+}, false);
+
+animate();
